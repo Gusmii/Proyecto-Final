edd698b9ee810ac6e8790dcc3b41148f7bdd2b87
EukenePadilla <tukeplayer@gmail.com>
2020-04-03 20:44:45 +0200

    NavBar terminado en cada vista

--- view/js/crearNavbarComponente.js
+++ view/js/crearNavbarComponente.js
@@ -1,29 +1,125 @@
+var paginas;
+var titulo;
+var htmlCode;
+var paginaActual;
+var nombrePaginas;
+var nombrePaginaActual;
 
 $(document).ready(function(){
-    console.log("hola");
-    $("#NavbarComponente").html(`<a class="navbar-brand" href="#" id="LinkImgLogo">
-    <img src="/view/img/logoBlancoEspacio.png" id="ImgLogo" alt="">
-  </a>
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">VIAJES</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">ESTANCIAS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">QUIENES SOMOS</a>
-        </li>
-        
-      </ul>
-      <span class="navbar-text">
-          <a class="nav-link" href="#">ENTRAR</a>
-        
-          <a class="nav-link" href="#">REGISTRARSE</a>
-      </span>
-    `);
+  var usuario=localStorage.setItem("usuario","admin");
 
+  titulo=window.location.pathname.split("/");
+  
+  paginas=["HOME","VIAJES","ESTANCIAS","ADMIN","QUIENESSOMOS"];
+  nombrePaginas=["index.html","vViajes.html","vEstancias.html","vAdmin.html","vQuienesSomos.html"];
+ 
+  if(titulo[1]=="index.html"){
+    paginaActual="HOME";
+    nombrePaginaActual="index.html";
+  }
+  
+  if(paginaActual!="HOME"){
+    nombrePaginaActual=titulo[2];
+    paginaActual=titulo[2].replace(".html","").slice(1).toUpperCase();
+  }
+  paginas = $.grep(paginas, function(value) {
+    return value != paginaActual;
+  });
+  nombrePaginas = $.grep(nombrePaginas, function(value) {
+    return value != nombrePaginaActual;
+  });
+  if(usuario=="normal"){
+    paginas = $.grep(paginas, function(value) {
+      return value != paginaActual;
+    });
+    nombrePaginas = $.grep(nombrePaginas, function(value) {
+      return value != nombrePaginaActual;
+    });
+  }
+   crearNavbar();
+  
 });
+function crearNavbar(){
+  var usuario=localStorage.getItem("usuario");
+  console.log("pagina actual");
+ console.log(paginaActual); 
+if(paginaActual!="HOME"){
+  htmlCode=`<a class="navbar-brand" href="../index.html" id="LinkImgLogo">`;
+  htmlCode+=`<img src="img/logoBlancoEspacio.png" id="ImgLogo" >`;
+ 
+}else{
+  htmlCode=`<a class="navbar-brand" href="index.html" id="LinkImgLogo">`;
+  htmlCode+=`<img src="view/img/logoBlancoEspacio.png" id="ImgLogo">`;
+}
+htmlCode+=` </a>`;
+
+htmlCode+=`<ul class="navbar-nav mr-auto">`;
+    for(var i=0;i<paginas.length;i++){
+      htmlCode+=`<li class="nav-item">`;
+
+      if(paginaActual!="HOME"){
+        if(paginas[i]=="HOME"){
+          htmlCode+=`<a class="nav-link" href="../`+nombrePaginas[i]+`">`+paginas[i]+`</a>`;
+        }else{
+          if(paginas[i]=="QUIENESSOMOS"){
+            htmlCode+=`<a class="nav-link" href="`+nombrePaginas[i]+`">QUIENES SOMOS</a>`;
+          }else{
+            if(usuario!="admin"){
+              htmlCode+=``;
+            }else{
+              htmlCode+=`<a class="nav-link" href="`+nombrePaginas[i]+`">`+paginas[i]+`</a>`;
+            }
+          }
+        }
+      }else{
+        if(paginas[i]=="QUIENESSOMOS"){
+          htmlCode+=`<a class="nav-link" href="view/`+nombrePaginas[i]+`">QUIENES SOMOS</a>`;
+        }else{
+          if(usuario!="admin" && paginas[i]!="ADMIN"){
+            htmlCode+=``;
+          }else{
+            htmlCode+=`<a class="nav-link" href="view/`+nombrePaginas[i]+`">`+paginas[i]+`</a>`;
+          }
+        }
+      }        
+      htmlCode+=`</li>`;
+    }
+
+    htmlCode+= `</ul>
+    <span class="navbar-text">
+        <a class="nav-link" href="#">ENTRAR</a>
+      
+        <a class="nav-link" href="#">REGISTRARSE</a>
+    </span>`;
+  $("#NavbarComponente").html(htmlCode);
+}
+
+
+// function crearNavbar(){console.log("hola");
+// $("#NavbarComponente").html(`<a class="navbar-brand" href="#" id="LinkImgLogo">
+// <img src="view/img/logoBlancoEspacio.png" id="ImgLogo" alt="">
+// </a>
+//   <ul class="navbar-nav mr-auto">
+//     <li class="nav-item active">
+//       <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+//     </li>
+
+    
+//     <li class="nav-item">
+//       <a class="nav-link" href="view/vViajes.html">VIAJES</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="view/vEstancias.html">ESTANCIAS</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="view/vQuienesSomos.html">QUIENES SOMOS</a>
+//     </li>
+    
+//   </ul>
+//   <span class="navbar-text">
+//       <a class="nav-link" href="#">ENTRAR</a>
+    
+//       <a class="nav-link" href="#">REGISTRARSE</a>
+//   </span>
+// `);
+// }
